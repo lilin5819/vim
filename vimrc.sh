b1652f0a78cda5cf707e0a4decb340665c395224
lilin5819 <1657301947@qq.com>
2016-07-20 23:37:11 +0800

    ok

--- vimrc.sh
+++ vimrc.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 cd ~
-#sudo apt-get install vim wget curl vim-addon-manager vim-runtime vim-youcompleteme
-#
-#curl https://j.mp/spf13-vim3 -L > spf13-vim.sh && sh spf13-vim.sh
-#[ $? -eq 0 ] || sh <(curl https://j.mp/spf13-vim3 -L)
-#
-#git clone https://github.com/powerline/fonts && [ -d fonts ] && sudo ./fonts/install 
-#[ $? -eq 0 ] echo "install powerline fonts successful!" || echo "error install powerline fonts!"
-#
-#git clone https://github.com/lilin5819/vimzsh
-#[ -d vimzsh ] && ln -s vimzsh/.vimrc* ~
-#[ $? -eq 0 ] && echo "ln -s .vimrc* successful!" || echo "error in ln .vimrc*"
-#[ -e vimzsh/agnosterzak.zsh-theme ] && ln -s vimzsh/agnosterzak.zsh-theme .oh-my-zsh/custom/
-echo "test ok"
+sudo apt-get install vim wget curl vim-addon-manager vim-runtime vim-youcompleteme
+
+curl https://j.mp/spf13-vim3 -L > spf13-vim.sh && sh spf13-vim.sh
+[ $? -eq 0 ] || sh <(curl https://j.mp/spf13-vim3 -L)
+
+git clone https://github.com/powerline/fonts && [ -d fonts ] && sudo ./fonts/install 
+[ $? -eq 0 ] echo "install powerline fonts successful!" || echo "error install powerline fonts!"
+
+git clone https://github.com/lilin5819/vimzsh
+[ -d vimzsh ] && ln -s vimzsh/.vimrc* ~
+[ $? -eq 0 ] && echo "ln -s .vimrc* successful!" || echo "error in ln .vimrc*"
+[ -e vimzsh/agnosterzak.zsh-theme ] && ln -s vimzsh/agnosterzak.zsh-theme .oh-my-zsh/custom/
