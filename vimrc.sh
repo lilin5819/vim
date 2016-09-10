381b18f2b0cfdb1ba6a68dda4973d80af7e275cb
lilin5819 <1657301947@qq.com>
2016-09-10 13:59:04 +0800

    change vimzsh.sh

--- vimrc.sh
+++ vimrc.sh
@@ -1,21 +1,32 @@
 #!/bin/bash
-cd ~
-sudo apt-get install vim wget curl vim-addon-manager vim-runtime vim-youcompleteme
 
-curl https://j.mp/spf13-vim3 -L > spf13-vim.sh && sh spf13-vim.sh
-[ $? -eq 0 ] || sh <(curl https://j.mp/spf13-vim3 -L)
+inst_vimdeps()
+{
+    sudo apt-get install vim git wget curl \
+        vim-addon-manager vim-scripts vim-runtime vim-youcompleteme >> ~/vimzsh.log
+    vam install youcompleteme
+    [ $? != 0 ] && echo '下载vim依赖失败'
+}
 
-git clone https://github.com/powerline/fonts && [ -d fonts ] && sudo ./fonts/install 
-[ $? -eq 0 ] echo "install powerline fonts successful!" || echo "error install powerline fonts!"
+conf_vim()
+{
+    curl https://j.mp/spf13-vim3 -L > spf13-vim.sh &&\
+        sh spf13-vim.sh || sh <(curl https://j.mp/spf13-vim3 -L) >> ~/vimzsh.log
 
-git clone https://github.com/lilin5819/vimzsh
-[ -d vimzsh ] && ln -s vimzsh/.vimrc* ~
-[ $? -eq 0 ] && echo "ln -s .vimrc* successful!" || echo "error in ln .vimrc*"
-[ -e vimzsh/agnosterzak.zsh-theme ] && ln -s vimzsh/agnosterzak.zsh-theme .oh-my-zsh/themes
+    [ $? != 0 ] && echo 'fork配置失败'
+}
 
-sudo apt-get install vim-runtime vim-scripts vim-youcompleteme
-vam install youcompleteme && grep 'let g:spf13_bundle_g.*youcsompleteme' vimzsh/.vimrc.before.local || echo "let g:spf13_bundle_groups=['general', 'programming', 'misc', 'scala', 'youcompleteme']">> vimzsh/.vimrc.before.local
-sudo ln -sf vimzsh/ycm_extra_conf.py /usr/lib/vim-youcompleteme/ycm_extra_conf.py
-echo "请在vim中输入 :BundleInstall"
 
-sed -i 's/<C-j>/<C-n>n/g' .vim/bundle/vim-plugins/plugin/*
+conf_vimlocal()
+{
+    git clone https://github.com/powerline/fonts && [ -d fonts ] && sudo ./fonts/install.sh
+    git clone https://github.com/lilin5819/vimzsh
+    sudo ln -sf vimzsh/ycm_extra_conf.py /usr/lib/vim-youcompleteme/ycm_extra_conf.py
+    [ -d vimzsh ] && ln -s vimzsh/.vimrc* ~
+    [ $? != 0 ] && "vim私有配置失败"
+}
+
+[ $? == 0 ] && echo "请在vim中输入 :BundleInstall" \
+    || echo "配置失败，请查看~/vimzsh.log"
+
+#sed -i 's/<C-j>/<C-n>n/g' .vim/bundle/vim-plugins/plugin/*
